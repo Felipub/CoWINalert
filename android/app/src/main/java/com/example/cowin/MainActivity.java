ae1f0afce856d791b67c290d8ca2dac539e77963
juankaYes <cubilesdevega@gmail.com>
2021-05-21 15:50:33 +0530

    Android folder

---
+++ android/app/src/main/java/com/example/cowin/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.cowin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Objects from layout
+    EditText etZipcode, etDistrict, etState;
+    Button buttonZipcode, buttonCity;
+    // Storing the dta from objects
+    String district,state,zipcode;
+    // Others
+    Boolean searchZipcode;
+    private final long TIMER = 60*1000; //milliseconds
+    Intent intent;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etZipcode = (EditText) findViewById(R.id.zipcode);
+        etDistrict = (EditText) findViewById(R.id.district);
+        etState = (EditText) findViewById(R.id.state);
+        buttonZipcode = (Button)findViewById(R.id.buttonZipcode);
+        buttonCity = (Button)findViewById(R.id.buttonCity);
+
+        buttonZipcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cancel previous alarm and update with new parameters
+                if (alarmManager != null){
+                    alarmManager.cancel(pendingIntent);
+                }
+                zipcode = etZipcode.getText().toString();
+                // Intent needed to send and recieve information from Notification_reciever
+                // Notification_receiver =  class to implement the python script on server
+                //                          & creates the alarm that will appear on top
+                intent = new Intent(getApplicationContext(), Notification_receiver.class);
+                intent.setAction("ZIPCODE:"+ zipcode);
+                pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,TIMER,TIMER,pendingIntent);
+                if (pendingIntent != null && alarmManager != null) {
+                    alarmManager.cancel(pendingIntent);
+                }
+
+            }
+
+        });
+
+        buttonCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cancel previous alarm and update with new parameters
+                if (alarmManager != null){
+                    alarmManager.cancel(pendingIntent);
+                }
+                district = etDistrict.getText().toString();
+                state = etState.getText().toString();
+
+            }
+        });
+    }
+    protected void setAlarm(String dataSend){
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
---
+++ android/app/src/main/java/com/example/cowin/Notification_receiver.java
@@ -0,0 +1,52 @@
+package com.example.cowin;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.ArrayList;
+
+public class Notification_receiver extends BroadcastReceiver  {
+    ArrayList<String> dataServer;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        String action = intent.getAction();
+        if (action.contains("ZIPCODE")){
+            String zipcode = action.split(":")[1];
+            dataServer = this.sendPetition(zipcode);
+        }
+        else{
+            String state = action.split("/")[0];
+            String district = action.split("/")[1];
+            dataServer = this.sendPetition(state,district);
+        }
+        // Needed a conditional to either crete notification or not depending on
+        // whether was found a slot for vaccination or not → I guess
+
+        Notification.Builder notification = new Notification.Builder(context)
+                .setSmallIcon(R.drawable.vaccine_icon)
+                .setContentTitle("Vaccine Available")
+                .setContentText("Please check the app to find where you can get the vaccine")
+                .setAutoCancel(true);
+
+        notificationManager.notify(100,notification.build());
+    }
+    private ArrayList<String> sendPetition(String zipcode){
+        ArrayList<String> data = new ArrayList<String>();
+        //Excute petition to server to execute python script
+        return data;
+    }
+    private ArrayList<String> sendPetition(String state, String district){
+        ArrayList<String> data = new ArrayList<String>();
+        //Excute petition to server to execute python script
+        return data;
+    }
+
+}
