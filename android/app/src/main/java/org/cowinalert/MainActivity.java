ea0a7fe9abd4e7314ee407510f4f31964a830ef6
Daniel Cuadrado Sánchez <danielcuadrado@spotlab.org>
2021-05-23 13:34:15 +0200

    hello world Firestore

--- android/app/src/main/java/org/cowinalert/MainActivity.java
+++ android/app/src/main/java/org/cowinalert/MainActivity.java
@@ -4,13 +4,24 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "CowinAlarm:::";
     //Objects from layout
     EditText etZipcode, etDistrict, etState;
     Button buttonZipcode, buttonCity;
@@ -70,6 +81,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+        //hello world firestore
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println(":::!");
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println(":::no");
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
+
+
     }
     protected void setAlarm(String dataSend){
 
